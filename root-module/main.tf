a444e5019f88bcc6cc40b0bddaa8d8ea6e3cc310
Kapil Thangavelu <kapilt@gmail.com>
2022-11-19 07:43:32 -0500

    enable tags on sqs in root

--- root-module/main.tf
+++ root-module/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_sqs_queue" "test_sqs" {
   name = uuid()
   sqs_managed_sse_enabled = true
-#  tags = {
-#    Environment = "Dev"
-#  }
+  tags = {
+    Environment = "Dev"
+  }
 }
