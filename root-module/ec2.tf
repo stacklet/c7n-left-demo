e0bc48e2215ae5419f5df3efffcdf1ed86911e7f
Kapil Thangavelu <kapilt@gmail.com>
2024-02-21 09:34:12 -0500

    add new ec2 instance

---
+++ root-module/ec2.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "m6g.large"
+
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_type = "gp2"
+    volume_size = "30"
+  }
+  
+  tags = {
+    Name = "HelloWorld"
+    Env  = "Dev"
+  }
+
+}
